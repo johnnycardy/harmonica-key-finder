efe15e05e55871fbc3b33864295261b3bfd72ad2
Jonathan Cardy <jcardy@gmail.com>
2013-06-25 21:27:17 +0100

    Switched to new version of cordova....properly

---
+++ android-package/gen/com/johnnycardy/keyfinder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.johnnycardy.keyfinder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- android-package/gen/com/johnnycardy/keyfinder/R.java
+++ android-package/gen/com/johnnycardy/keyfinder/R.java
@@ -17,6 +17,9 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+    }
+    public static final class xml {
+        public static final int config=0x7f040000;
     }
 }
